336945a0c03d1ca92125a9183c97cc923facf609
MeetNavdiya1 <meetnavadiya7272@gmail.com>
2026-07-07 13:21:48 +0530

    Fix Express 5 catch-all route

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,7 +43,7 @@ app.get("/health", (_req, res) => {
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
